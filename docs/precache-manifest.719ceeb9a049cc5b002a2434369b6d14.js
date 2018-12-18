ae55c7e97c45046c5668aaa85e0fbefde3e0cbbe
Toufiq <burningraven06@gmail.com>
2018-12-18 16:56:02 +0600

    create docs

---
+++ docs/precache-manifest.719ceeb9a049cc5b002a2434369b6d14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f3fb7456a7b7e008fac",
+    "url": "/static/js/main.4f3fb745.chunk.js"
+  },
+  {
+    "revision": "b1b62b2a55412c2b94da",
+    "url": "/static/js/1.b1b62b2a.chunk.js"
+  },
+  {
+    "revision": "7c2d27bd711a0cd75f8e3351ad0249f3",
+    "url": "/index.html"
+  }
+];
